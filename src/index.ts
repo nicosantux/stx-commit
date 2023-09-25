b4c3e7920c2b81a121aacf02fdb3c50e04d46935
Nicolás Santuccio <nico.santuccio@gmail.com>
2023-09-24 22:13:37 -0300

    feat: add starter menu

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,40 @@
 #!/usr/bin/env node
 
-// TODO IMPLEMENT
+import { intro, select } from '@clack/prompts'
+import color from 'picocolors'
 
-// eslint-disable-next-line no-console
-console.log('Santux Commit')
+import { handleCancelPrompt } from './utils/index.js'
+import { Option } from './types/index.js'
+
+intro(color.white(' Welcome to Santux Commit '))
+
+type Action = 'add' | 'remove' | 'commit' | 'quit' | 'status'
+
+let action: Action
+
+do {
+  action = handleCancelPrompt(
+    await select<Option<Action>[], Action>({
+      message: 'What do you want to do?',
+      options: [
+        { label: 'Files Status', value: 'status' },
+        { label: 'Add files', value: 'add' },
+        { label: 'Remove files', value: 'remove' },
+        { label: 'Commit files', value: 'commit' },
+        { label: 'Quit', value: 'quit' },
+      ],
+    }),
+  )
+
+  if (action === 'status') {
+    console.log('show status')
+  }
+
+  if (action === 'add') {
+    console.log('show add files')
+  }
+
+  if (action === 'remove') {
+    console.log('show remove files')
+  }
+} while (action !== 'commit' && action !== 'quit')
