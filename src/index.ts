1cc4ff52a23b7b736105f8c4bbffe47569e8a4fd
Nicolás Santuccio <nico.santuccio@gmail.com>
2023-10-24 21:51:58 +0200

    feat: create commit via cli

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,114 @@
 #!/usr/bin/env node
 
-import { intro, select } from '@clack/prompts'
+import type { Action, GitStatus, Option } from './types/index.js'
+
+import { intro, outro, select } from '@clack/prompts'
 import color from 'picocolors'
 
-import { handleCancelPrompt } from './utils/index.js'
-import { Option } from './types/index.js'
+import { MENU_OPTIONS } from './constants/index.js'
+import {
+  NotInGitRepositoryError,
+  createCommit,
+  getStatusFiles,
+  handleCancelPrompt,
+} from './utils/index.js'
+import {
+  showFilesToRestore,
+  showFilesStatus,
+  showFilesToAdd,
+  getCommitType,
+  getCommitScope,
+  getCommitTitle,
+  getCommitBodyMessage,
+  getCommitFooter,
+  getIsBreakingChange,
+  buildCommit,
+  showConfirmCommit,
+} from './lib/index.js'
 
 intro(color.white(' Welcome to Santux Commit '))
 
-type Action = 'add' | 'remove' | 'commit' | 'quit' | 'status'
+let status: GitStatus = { staged: [], notStaged: [], untracked: [] }
 
 let action: Action
 
 do {
+  try {
+    status = await getStatusFiles()
+  } catch (error) {
+    if (!(error instanceof NotInGitRepositoryError)) {
+      outro('An error occured. Please try again.')
+      process.exit(1)
+    }
+
+    status = await getStatusFiles()
+  }
+
   action = handleCancelPrompt(
-    await select<Option<Action>[], Action>({
+    await select<Option<Partial<Action>>[], Action>({
       message: 'What do you want to do?',
-      options: [
-        { label: 'Files Status', value: 'status' },
-        { label: 'Add files', value: 'add' },
-        { label: 'Remove files', value: 'remove' },
-        { label: 'Commit files', value: 'commit' },
-        { label: 'Quit', value: 'quit' },
-      ],
+      options: MENU_OPTIONS.filter((option) => {
+        if (option.value === 'add' && !status.notStaged.length && !status.untracked.length) {
+          return false
+        }
+
+        if ((option.value === 'restore' || option.value === 'commit') && !status.staged.length) {
+          return false
+        }
+
+        return true
+      }),
     }),
   )
 
   if (action === 'status') {
-    console.log('show status')
+    showFilesStatus(status)
   }
 
   if (action === 'add') {
-    console.log('show add files')
+    await showFilesToAdd(
+      status.notStaged.concat(status.untracked.map((file) => `untracked: ${file}`)),
+    )
   }
 
-  if (action === 'remove') {
-    console.log('show remove files')
+  if (action === 'restore') {
+    await showFilesToRestore()
   }
 } while (action !== 'commit' && action !== 'quit')
+
+if (action === 'quit') {
+  outro('👋 Bye')
+  process.exit(0)
+}
+
+const commitType = await getCommitType()
+
+const scope = await getCommitScope()
+
+const commitTitle = await getCommitTitle({ commitType, scope })
+
+const bodyMessage = await getCommitBodyMessage()
+
+const commitFooter = await getCommitFooter()
+
+const isBreakingChange = await getIsBreakingChange()
+
+const commit = buildCommit({
+  bodyMessage,
+  commitFooter,
+  commitTitle,
+  commitType,
+  isBreakingChange,
+  scope,
+})
+
+const isCommitConfirmed = await showConfirmCommit(commit)
+
+if (!isCommitConfirmed) {
+  outro('Your commit has been canceled')
+  process.exit(0)
+}
+
+await createCommit(commit)
+
+outro('✅ Commit created.')
